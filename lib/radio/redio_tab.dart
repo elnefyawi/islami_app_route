88ac8b799fc031f797ca4808eab8440af116a6d1
MAHMOUD ELNEFYAWI <mahmoudelnefyawi184@gmail.com>
2023-09-22 22:29:59 +0300

    end_hadeth_tab

--- lib/radio/redio_tab.dart
+++ lib/radio/redio_tab.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 class RedioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+            margin:
+                EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.13),
+            child: Image.asset('assets/images/radio_logo.png')),
+        Container(
+            margin: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.04,
+              bottom: MediaQuery.of(context).size.height * 0.04,
+            ),
+            child: Text(
+              'اذاعة القران الكريم',
+              style: Theme.of(context).textTheme.titleMedium,
+              textAlign: TextAlign.center,
+            )),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(
+              Icons.skip_previous,
+              size: 35,
+            ),
+            Icon(Icons.play_arrow, size: 50),
+            Icon(
+              Icons.skip_next,
+              size: 35,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
