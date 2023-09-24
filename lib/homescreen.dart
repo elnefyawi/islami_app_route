1cdf6f48895f28c1f84b85d3c7e3240b5dc2e9b3
MAHMOUD ELNEFYAWI <145044688+elnefyawi@users.noreply.github.com>
2023-09-24 05:07:56 +0300

    Revert "Feature end islami app"

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,90 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:islami_app_route_1/hadeth/hadeth_tab.dart';
-import 'package:islami_app_route_1/provider/app_config_provider.dart';
-import 'package:islami_app_route_1/quran/quran_tab.dart';
-import 'package:islami_app_route_1/radio/redio_tab.dart';
-import 'package:islami_app_route_1/sebha/sebha_tab.dart';
-import 'package:islami_app_route_1/settings/settings.dart';
-import 'package:provider/provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   static const String routeName = 'HomeScreen';
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int selectedIndex = 0;
-
-  @override
   Widget build(BuildContext context) {
-    var provider = Provider.of<AppConfigProvider>(context);
-    return Stack(
-      children: [
-        provider.isDarkMode()
-            ? Image.asset(
-                'assets/images/main_back_ground_dark.png',
-                width: double.infinity,
-                height: double.infinity,
-                fit: BoxFit.fill,
-              )
-            : Image.asset(
-                'assets/images/main_background.png',
-                width: double.infinity,
-                height: double.infinity,
-                fit: BoxFit.fill,
-              ),
-        Scaffold(
-          appBar: AppBar(
-            title: Text(
-              AppLocalizations.of(context)!.app_title,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ),
-          bottomNavigationBar: Theme(
-            data: Theme.of(context)
-                .copyWith(canvasColor: Theme.of(context).primaryColor),
-            child: BottomNavigationBar(
-                currentIndex: selectedIndex,
-                onTap: (index) {
-                  selectedIndex = index;
-                  setState(() {});
-                },
-                items: [
-                  BottomNavigationBarItem(
-                      icon:
-                          ImageIcon(AssetImage('assets/images/icon_quran.png')),
-                      label: AppLocalizations.of(context)!.quran),
-                  BottomNavigationBarItem(
-                      icon: ImageIcon(
-                          AssetImage('assets/images/icon_hadeth.png')),
-                      label: AppLocalizations.of(context)!.hadeth),
-                  BottomNavigationBarItem(
-                      icon:
-                          ImageIcon(AssetImage('assets/images/icon_sebha.png')),
-                      label: AppLocalizations.of(context)!.sebha),
-                  BottomNavigationBarItem(
-                      icon:
-                          ImageIcon(AssetImage('assets/images/icon_radio.png')),
-                      label: AppLocalizations.of(context)!.radio),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.settings),
-                      label: AppLocalizations.of(context)!.settings),
-                ]),
-          ),
-          body: tabs[selectedIndex],
-        )
-      ],
-    );
+    return Scaffold();
   }
-
-  List<Widget> tabs = [
-    QuranTab(),
-    HadethTab(),
-    SebhaTab(),
-    RedioTab(),
-    SettingsTab()
-  ];
 }
